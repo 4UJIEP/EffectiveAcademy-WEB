6b7e11bec8c91a59ab8f8d9773b6b6d4d7b37c4b
Alexey <simindey.alexey@gmail.com>
2024-11-28 14:44:46 +0600

    Website structure update

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import { BrowserRouter, Navigate, useRoutes} from 'react-router-dom'
+import './index.scss'
+import Characters from './pages/Characters.tsx'
+import NotFoundPage from './pages/NotFoundPage.tsx'
+import Comics from './pages/Comics.tsx'
+import CharacterPage from './pages/CharacterPage.tsx'
+import ComicPage from './pages/ComicPage.tsx'
+import Layout from './pages/Layout.tsx'
+
+function AppRoutes()
+{
+  const routes = [
+    {
+      path: "/",
+      element: <Layout/>,
+      errorElement: <NotFoundPage />,
+      children: [
+        {
+          path: "/", 
+          element: <Navigate to="/characters" replace/>
+        },
+        {
+          path: "/characters", 
+          element: <Characters />
+        },
+        {
+          path: '/characters/:characterId',
+          element: <CharacterPage />,
+        },
+        {
+          path: '/comics',
+          element: <Comics />,
+        },
+        {
+          path: '/comics/:comicId',
+          element: <ComicPage />,
+        }
+      ]
+    }
+  ];
+  return useRoutes(routes);
+}
+
+function App()
+{
+  return (<BrowserRouter><AppRoutes /></BrowserRouter>);
+}
+
+export default App;
