b80a7e1720abf97658a1002635f46c058c501bf0
Alexey <simindey.alexey@gmail.com>
2024-12-09 20:20:31 +0600

    New loading

---
+++ src/components/loading.tsx
@@ -0,0 +1,14 @@
+export default function Loading()
+{
+    return (
+        <div className="loading">
+            <span/>
+            <span/>
+            <span/>
+            <span/>
+            <span/>
+            <span/>
+            <span/>
+        </div>
+    );
+}
